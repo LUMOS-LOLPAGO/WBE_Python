ca78af07d2fefb0642a3e45cf1a1d94237496dae
Doyun Kim <roger428@naver.com>
2025-07-24 13:20:58 +0900

    refactor: WBE_Spring 서버 URL 주소로 리펙토링

--- worker/stt/stt_worker_process.py
+++ worker/stt/stt_worker_process.py
@@ -62,7 +62,7 @@ def whisper_pipeline(summoner_id, region, audio_data, audio_queue, loop):
     print(f"[🎯 결과] {summoner_id}: {final_text}")
 
     # Spring 서버로 결과 전송 ([챔피언 이름] [스펠 이름])
-    response = requests.post("http://localhost:8080/spell", json={
+    response = requests.post("https://lolpago.com/spell", json={
         "summonerId": summoner_id,
         "finalText": final_text,
         "region": region
@@ -91,7 +91,7 @@ def whisper_pipeline(summoner_id, region, audio_data, audio_queue, loop):
 
     # 스펠 쿨다운이 끝났을 때 알람 메세지 요청
     try:
-        cooldown_response = requests.get("http://localhost:8080/spell/await",
+        cooldown_response = requests.get("https://lolpago/spell/await",
                                          params=spell_cool_down_params,
                                          timeout=360)
 
